5a27d72b5d8535f2dc06726d5c4ccbe628db4987
junwen_su <517897442@qq.com>
2017-01-19 15:39:34 +0800

    上拉加载更多

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,7 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    swiperData: [
-      { name: "../../images/YZDP-H5-750x2445_01.jpg", id: 1},
-      { name: "../../images/banner.png", id: 2, bgColor: 'red' },
-      { name: "../../images/组-10.png", id: 3, bgColor: 'yellow' },
-      { name: "../../images/flower.jpg", id: 4, bgColor: 'white' },
+    swiperData: [{ name: "../../images/YZDP-H5-750x2445_01.jpg", id: 1 ,bgColor: 'red'}, { name: "../../images/banner.png", id: 2, bgColor: 'red' }, { name: "../../images/组-10.png", id: 3, bgColor: 'yellow' }, { name: "../../images/flower.jpg", id: 4, bgColor: 'white' },
     ],
     poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
     name: '此时此刻',
@@ -16,38 +12,27 @@ Page({
     src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
     userInfo: {},
     goodsKindDetails1: [
-      {
-        name: "kind1",
-        img: "img1"
-      },
-      {
-        name: "kind2",
-        img: "img2"
-      },
-      {
-        name: "kind3",
-        img: "img3"
-      }
+      { name: "kind1", img: "img1" },
+      { name: "kind2", img: "img2" },
+      { name: "kind3", img: "img3" }
     ],
     goodsKindDetails2: [
-      {
-        name: "kind4",
-        img: "img4"
-      },
-      {
-        name: "kind5",
-        img: "img5"
-      },
-      {
-        name: "kind6",
-        img: "img6"
-      }
+      { name: "kind4", img: "img4" },
+      { name: "kind5", img: "img5" },
+      { name: "kind6", img: "img6" }
     ],
-    longItemList: []
+    longItemList: [],
+    showLoading: false,
   },
   onPullDownRefresh() {
     wx.showToast({
       title: '正在加载中...'
+    })
+  },
+  onReachBottom() {
+    wx.showToast({
+      title: "loading more , please wait",
+      icon: "loading"
     })
   },
   getLocation() {
@@ -66,7 +51,7 @@ Page({
   },
   setLongItemList() {
     let items = [];
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
       items.push({ name: "item" + i })
     };
     this.setData({
@@ -80,22 +65,22 @@ Page({
     // 使用 wx.createAudioContext 获取 audio 上下文 context
     this.audioCtx = wx.createAudioContext('myAudio')
   },
-  audioPlay: function () {
-    this.audioCtx.play()
-  },
-  audioPause: function () {
-    this.audioCtx.pause()
-  },
-  audio14: function () {
-    this.audioCtx.seek(14)
-  },
-  audioStart: function () {
-    this.audioCtx.seek(0)
-  },
-  loadMore(){
+  onReachBottom(){
+    let items = [];
+    this.setData({
+      showLoading:true
+    });
+    for(let i = this.data.longItemList.length +1;i<=this.data.longItemList.length+10;i++){
+      items.push({name:"item"+i});
+    }
+    
+    this.setData({
+      showLoading:false,
+      longItemList:this.data.longItemList.concat(items)
+    })    
     wx.showToast({
-      title:"loading",
-      icon:"loading"
+      title: "loading",
+      icon: "loading"
     })
   }
 
